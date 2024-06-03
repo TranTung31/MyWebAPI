69612a338f00782af718203bf334abafcbffb7b5
Your Name <you@example.com>
2024-06-03 10:15:09 +0700

    feat: entity framework core (code first)

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebAPI.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+        #region DbSet
+        public DbSet<HangHoa> HangHoa { get; set; }
+        public DbSet<Loai> Loai { get; set; }
+        #endregion
+    }
+}
