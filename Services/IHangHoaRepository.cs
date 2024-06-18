3ece3a5b45636c754bd72412cec832924ff38872
Your Name <you@example.com>
2024-06-18 23:49:52 +0700

    feat: filtering, sorting, paging

---
+++ Services/IHangHoaRepository.cs
@@ -0,0 +1,14 @@
+﻿using MyWebAPI.Models;
+using System.Collections.Generic;
+
+namespace MyWebAPI.Services
+{
+    public interface IHangHoaRepository
+    {
+        List<HangHoaModel> GetAll(string search, double? from, double? to, string sortBy, int page = 1);
+        HangHoaModel GetById(string id);
+        HangHoa Create(HangHoaVM hangHoa);
+        HangHoa Update(string id, HangHoaVM hangHoa);
+        void Delete(string id);
+    }
+}
